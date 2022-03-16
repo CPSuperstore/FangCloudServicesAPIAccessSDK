a6903f2bddc12742fb02eeaa41d2d16641bae7a9
Christopher <cpsuperstoreinc@gmail.com>
2022-03-16 02:37:54 -0400

    Added PIP related files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='FangCloudServicesAPIAccessSDK',
+    version='1.0',
+    packages=['FCSAPIAccess'],
+    url='https://github.com/CPSuperstore/FangCloudServicesAPIAccessSDK',
+    license='Apache License 2.0',
+    author='CPSuperstore',
+    author_email='cpsuperstoreinc@gmail.com',
+    description='The SDK for project-level access to your FangCloudServices account'
+)
